25aed79942ba369ed8d0dda0e0c30f97f7132d0c
Julien Vitay <julien.vitay@informatik.tu-chemnitz.de>
2019-02-24 22:42:06 +0100

    Rebuilding site So 24. Feb 22:42:03 CET 2019

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Message for the commit
+msg="Rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+
+# Fetch the latest update to static/deeprl
+echo -e "\033[0;32mUpdating static/deeprl...\033[0m"
+git submodule update --remote static/deeprl
+
+
+# Build the project.
+hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+
+# Push all local changes to github
+echo -e "\033[0;32mPush local changes to GitHub...\033[0m"
+git commit -a -m "$msg"
+git push origin master
